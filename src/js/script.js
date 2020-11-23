dfda682ffc80c4d293d957015a218edb9080bc8e
sergey5130 <sergeyprok5130@gmail.com>
2020-11-23 18:54:07 +0200

    +

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"> <img src="../img/ar1.png"> </button>',
-        nextArrow: '<button type="button" class="slick-next"> <img src="../img/ar2.png"> </button>',
+        prevArrow: '<button type="button" class="slick-prev"> <img src="img/ar1.png"> </button>',
+        nextArrow: '<button type="button" class="slick-next"> <img src="img/ar2.png"> </button>',
         responsive: [
             {
                 breakpoint: 992,
