bb8feb8d5d4164783427804e53efd9d860748c97
sergey5130 <sergeyprok5130@gmail.com>
2020-11-27 13:12:34 +0200

    valid

--- src/js/script.js
+++ src/js/script.js
@@ -33,4 +33,56 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+            //modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    }); 
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+           $('#order .modal__drscr').text($('.catalog-item__subtitle').eq(i).text());
+           $('.overlay, #order').fadeIn('slow');
+        }); 
+    });
+
+    //validation
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+    
 });
+
